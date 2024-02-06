f897ce2efe3c0f2febdea5d456172afb0a20d728
Derek <109226053+derekjtong@users.noreply.github.com>
2024-02-06 07:16:54 -0500

    Add speed updates

---
+++ src/truck.rs
@@ -0,0 +1,90 @@
+use crate::{road_item::{DynamicRoadItem, Point, RoadItem}, road_items_dynamic::Vehicle, utils::Constants};
+
+// TODO: consolidate into folder
+
+pub struct Truck {
+    base: DynamicRoadItem,
+    model: String,
+    speed: f64,
+    direction: f64,
+    desired_speed: f64,
+    load_weight: f64, // tons
+}
+
+impl Truck {
+    pub fn new(x: f64, y: f64, model: String, speed: f64, direction: f64, desired_speed: f64, load_weight: f64) -> Self {
+        Self {
+            base: DynamicRoadItem::new(x, y),
+            model,
+            speed,
+            direction,
+            desired_speed,
+            load_weight,
+        }
+    }
+}
+
+
+impl RoadItem for Truck {
+    fn set_pos(&mut self, pos: Point) {
+        self.base.set_pos(pos);
+    }
+
+    fn pos(&self) -> Point {
+        self.base.pos()
+    }
+
+    fn type_name(&self) -> &'static str {
+        "Truck"
+    }
+}
+
+impl Vehicle for Truck {
+    fn new(x: f64, y: f64, model: String, speed: f64, direction: f64, desired_speed: f64) -> Self where Self: Sized {
+        Self::new(x, y, model, speed, direction, desired_speed, 0.0) // Default load_weight to 0.0
+    }
+
+    fn model(&self) -> &String {
+        &self.model
+    }
+
+    fn set_desired_speed(&mut self, mph: f64) {
+        self.desired_speed = mph;
+    }
+
+    fn get_current_speed(&self) -> f64 {
+        self.speed
+    }
+
+    fn update_speed(&mut self, seconds: i32) {
+        if self.speed < self.desired_speed {
+            self.accelerate(seconds);
+        } else if self.speed > self.desired_speed {
+            self.decelerate(seconds);
+        }
+    }
+
+    fn accelerate(&mut self, seconds: i32) {
+        let acceleration_rate = if self.load_weight <= 5.0 {
+            Constants::ACC_RATE_EMPTY
+        } else {
+            Constants::ACC_RATE_FULL
+        };
+        self.speed += acceleration_rate * seconds as f64;
+        if self.speed > self.desired_speed {
+            self.speed = self.desired_speed;
+        }
+    }
+
+    fn decelerate(&mut self, seconds: i32) {
+        let deceleration_rate = if self.load_weight <= 5.0 {
+            Constants::DEC_RATE_EMPTY
+        } else {
+            Constants::DEC_RATE_FULL
+        };
+        self.speed -= deceleration_rate * seconds as f64;
+        if self.speed < self.desired_speed {
+            self.speed = self.desired_speed;
+        }
+    }
+}
---
+++ src/utils.rs
@@ -0,0 +1,11 @@
+pub struct Constants;
+
+impl Constants {
+    pub const ACC_RATE: f64 = 3.5; // Acceleration rate for cars in m/s
+    pub const ACC_RATE_EMPTY: f64 = 2.5; // Acceleration rate for light trucks in m/s
+    pub const ACC_RATE_FULL: f64 = 1.0; // Acceleration rate for heavy trucks in m/s
+    pub const DEC_RATE: f64 = 7.0; // Braking rate for cars in m/s
+    pub const DEC_RATE_EMPTY: f64 = 5.0; // Braking rate for light trucks in m/s
+    pub const DEC_RATE_FULL: f64 = 2.0; // Braking rate for heavy trucks in m/s
+    pub const MPS_TO_MPH: f64 = 2.237;
+}
