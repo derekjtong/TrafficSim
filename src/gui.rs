525608b8e871aef345c91bdd604765ec5b6720ae
Derek <109226053+derekjtong@users.noreply.github.com>
2024-02-06 20:37:31 -0500

    Add gui

---
+++ src/gui.rs
@@ -0,0 +1,29 @@
+use crate::map::Map; 
+
+// TODO: decide on gui library
+pub struct Gui {
+    map: Map,
+}
+
+impl Gui {
+    pub fn new() -> Self {
+        Self { map: Map::new() }
+    }
+
+    pub fn add_road_through_gui(&mut self /*, road_parameters: ... */) {
+        // self.map.add_road(Road::new(/* road parameters */));
+        // TODO: imeplement road parameters in road.rs
+        println!("Placeholder: A road has been added.");
+    }
+
+    pub fn remove_road_through_gui(&mut self, index: usize) {
+        self.map.remove_road(index);
+        println!("Placeholder: A road at index {} has been removed, if it existed.", index);
+    }
+
+    pub fn display_map(&self) {
+        // TODO: placeholder for actual GUI display logic.
+        println!("Placeholder: Displaying map with {} roads.", self.map.get_roads().len());
+        println!("Total road items on the map: {}", self.map.total_road_items());
+    }
+}
