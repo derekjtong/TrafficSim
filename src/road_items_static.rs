a18e9e3893bdc89bc224fc9ebc3b7b7c352d6299
Derek <109226053+derekjtong@users.noreply.github.com>
2024-02-06 20:16:28 -0500

    Update constructor for SpeedLimitSign

--- src/road_items_static.rs
+++ src/road_items_static.rs
@@ -1,4 +1,4 @@
-use crate::road_item::StaticRoadItem;
+use crate::road_item::{StaticRoadItem, Point};
 
 // TODO: add RoadItem traits
 
@@ -16,9 +16,9 @@ pub struct SpeedLimitSign {
 }
 
 impl SpeedLimitSign {
-    pub fn new(speed_limit: i32) -> Self {
+    pub fn new(x: f64, y: f64,speed_limit: i32) -> Self {
         Self {
-            base: StaticRoadItem { pos: Point { x: 0.0, y: 0.0 } },
+            base: StaticRoadItem::new(x, y),
             speed_limit,
         }
     }
