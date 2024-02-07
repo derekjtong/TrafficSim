585e99a0a9d3934a3291f4edd3368ad34441c05b
Derek <109226053+derekjtong@users.noreply.github.com>
2024-02-06 20:16:45 -0500

    Add files to lib for usage in test directory

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub mod simulation;
+pub use simulation::Simulation;
+
+pub mod road;
+pub use road::*;
+
+pub mod road_item;
+pub use road_item::*;
+
+pub mod road_items_dynamic;
+pub use road_items_dynamic::*;
+
+pub mod road_items_static;
+pub use road_items_static::*;
+
+pub mod utils;
+pub use utils::*;
+
+pub mod car;
+pub use car::*;
+
+pub mod truck;
+pub use truck::*;
