6e01717546ef018941082cc3fd4c59ce9f12f5f1
Derek <109226053+derekjtong@users.noreply.github.com>
2024-02-21 05:23:01 -0500

    Update constants

--- src/utils.rs
+++ src/utils.rs
@@ -7,6 +7,12 @@ impl Constants {
     pub const DEC_RATE: f64 = 7.0; // Braking rate for cars in m/s
     pub const DEC_RATE_EMPTY: f64 = 5.0; // Braking rate for light trucks in m/s
     pub const DEC_RATE_FULL: f64 = 2.0; // Braking rate for heavy trucks in m/s
+
     pub const MPS_TO_MPH: f64 = 2.237;
-    pub const KPH_TO_MPH: f64 = 0.621371;
+    pub const MPH_TO_MPS: f64 = 0.44704;
+
+    pub const MPS_TO_KPH: f64 = 3.6;
+    pub const KPH_TO_MPS: f64 = 0.277778;
+
+    // pub const KPH_TO_MPH: f64 = 0.621371;
 }
