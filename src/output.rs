b4d1904cc72b5467de323678083a92e835e35299
Derek <109226053+derekjtong@users.noreply.github.com>
2024-02-21 14:13:03 -0500

    Fix formatting

--- src/output.rs
+++ src/output.rs
@@ -1,6 +1,6 @@
 pub trait SimOutput {
     fn get_speed(&self, vehicle_speed: f64) -> f64;
-    fn is_metric(&self)->bool;
+    fn is_metric(&self) -> bool;
 }
 
 pub struct ImperialOutput;
@@ -9,7 +9,7 @@ impl SimOutput for ImperialOutput {
     fn get_speed(&self, vehicle_speed: f64) -> f64 {
         vehicle_speed // Assuming speed is already in MPH
     }
-    fn is_metric(&self)->bool {
+    fn is_metric(&self) -> bool {
         false
     }
 }
@@ -20,7 +20,7 @@ impl SimOutput for MetricOutput {
     fn get_speed(&self, vehicle_speed: f64) -> f64 {
         vehicle_speed * 1.6 // Convert MPH to KPH
     }
-    fn is_metric(&self)->bool {
+    fn is_metric(&self) -> bool {
         true
     }
 }
