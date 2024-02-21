d858723a23fe349fb8cc24da7e47c53ef6d7b917
Derek <109226053+derekjtong@users.noreply.github.com>
2024-02-21 01:26:50 -0500

    Add unit identifier in SimOutput base trait

--- src/output.rs
+++ src/output.rs
@@ -1,5 +1,6 @@
 pub trait SimOutput {
     fn get_speed(&self, vehicle_speed: f64) -> f64;
+    fn is_metric(&self)->bool;
 }
 
 pub struct ImperialOutput;
@@ -8,6 +9,9 @@ impl SimOutput for ImperialOutput {
     fn get_speed(&self, vehicle_speed: f64) -> f64 {
         vehicle_speed // Assuming speed is already in MPH
     }
+    fn is_metric(&self)->bool {
+        false
+    }
 }
 
 pub struct MetricOutput;
@@ -15,5 +19,8 @@ pub struct MetricOutput;
 impl SimOutput for MetricOutput {
     fn get_speed(&self, vehicle_speed: f64) -> f64 {
         vehicle_speed * 1.6 // Convert MPH to KPH
+    }
+    fn is_metric(&self)->bool {
+        true
     }
 }
