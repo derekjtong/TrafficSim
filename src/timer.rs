90eadbb954b805911556dd85aaa584299d06bea3
Derek <109226053+derekjtong@users.noreply.github.com>
2024-04-23 11:14:50 -0400

    Implement timer

--- src/timer.rs
+++ src/timer.rs
@@ -1,11 +1,50 @@
+use std::thread;
+use std::time::Duration;
+
+use crate::{Drawable, IPrintDriver, Map, Simulation};
+
 pub struct Timer {
-    // TODO
+    simulation: Simulation,
+    map: Map,
+    print_driver: Box<dyn IPrintDriver>,
+    drawable: Box<dyn Drawable>,
 }
 
 impl Timer {
-    pub fn new() -> Self {
-        Timer {
-            // TODO
+    pub fn new(
+        simulation: Simulation,
+        map: Map,
+        print_driver: Box<dyn IPrintDriver>,
+        drawable: Box<dyn Drawable>,
+    ) -> Self {
+        Self {
+            simulation,
+            map,
+            print_driver,
+            drawable,
+        }
+    }
+
+    pub fn start(&mut self) {
+        let spinner = vec![".", "..", "...", ""];
+        let mut spinner_index = 0;
+        loop {
+            // Clear the screen
+            print!("{}[2J", 27 as char);
+
+            // Update simulation
+            self.simulation.update(1); // assuming 1 second has passed
+
+            // Print the map and dynamic road items
+            self.map.print(&*self.print_driver, &mut *self.drawable);
+            self.drawable.print();
+
+            // Update progress spinner
+            println!("{}", spinner[spinner_index]);
+            spinner_index = (spinner_index + 1) % spinner.len();
+
+            // Sleep for one second
+            thread::sleep(Duration::from_secs(1));
         }
     }
 }
